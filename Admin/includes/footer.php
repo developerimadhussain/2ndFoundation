fb94da4f2c35ac9c125c8105c9748b2a8766baa6
Imad Hussain <78842326+developerimadhussain@users.noreply.github.com>
2021-08-17 20:04:51 +0600

    Add files via upload

---
+++ Admin/includes/footer.php
@@ -0,0 +1,30 @@
+
+
+      <!-- Footer -->
+	  <br>
+	  <br>
+      <footer class="sticky-footer bg-white" style="color:black;">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright @ 2ND FOUNDATION 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+      </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+    </div>
+    <!-- End of Content Wrapper -->
+
+    <script src="js/wow.min.js"></script>
+				<script>
+				new WOW().init();
+				</script>
+				
+				   <script src="js/jquery-3.4.1.slim.min.js"></script>  
+</body>
+
+</html>
