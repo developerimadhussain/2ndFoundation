67f9eebe06d74d6fc59cdcf473f8e9afc2cb5be1
Imad Hussain <78842326+developerimadhussain@users.noreply.github.com>
2021-08-17 20:40:33 +0600

    Update header.php

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -14,7 +14,7 @@
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/animate.min.css">		
-</head>
+
 <body id="page-top">
   <!-- Page Wrapper -->
   <div id="wrapper">
